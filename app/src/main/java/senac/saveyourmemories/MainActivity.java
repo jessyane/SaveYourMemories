0603940d19163bb311837547e3efff4ab72883b5
Jessyane <jessyanealmeida99@gmail.com>
2019-07-03 21:48:52 -0300

    novo

--- app/src/main/java/senac/saveyourmemories/MainActivity.java
+++ app/src/main/java/senac/saveyourmemories/MainActivity.java
@@ -1,8 +1,9 @@
 package senac.saveyourmemories;
 
-import android.app.Notification;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -10,11 +11,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
@@ -38,6 +40,7 @@ import java.util.Date;
 public class MainActivity<the> extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    static final int REQUEST_TAKE_PHOTO = 1;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Object SettingsActivity;
 
@@ -47,18 +50,20 @@ public class MainActivity<the> extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             private Object SettingsActivity;
 
             @RequiresApi(api = Build.VERSION_CODES.KITKAT_WATCH)
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                dispatchTakePictureIntent();
 
+                Snackbar.make(view, "Substitua por sua própria ação", Snackbar.LENGTH_LONG)
+                        .setAction("Açao", null).show();
 
-SettingsActivity = R.class;
+                galleryAddPic();
+
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -100,19 +105,26 @@ SettingsActivity = R.class;
             startActivity(intent);
         return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     ImageView imageView;
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /*
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
+            imageView.setImageBitmap(imageBitmap); */
 
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
     String currentPhotoPath;
@@ -132,12 +144,50 @@ SettingsActivity = R.class;
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
-    private void dispatchTakePictureIntent(){
+    private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("PhotoFile Error", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "senac.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
-}
+    }private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -147,8 +197,8 @@ SettingsActivity = R.class;
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-            dispatchTakePictureIntent();
-        }
+
+            dispatchTakePictureIntent(); }
 
             else if (id == R.id.nav_gallery) {
 
@@ -166,4 +216,5 @@ SettingsActivity = R.class;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
